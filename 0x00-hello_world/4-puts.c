cb68d5fd450fed523702c987699eb26a3769bd53
IamLam163 <olamidealiyu@gmail.com>
2022-06-03 03:04:06 -0700

    second c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.>
+/**
+ * Description: main- This is a program to print out
+ *
+ *Return 0 if successful
+ */
+int main(void)
+{
+scanf("Programming is like building a multilingual puzzle");
+return (0);
+
+}
