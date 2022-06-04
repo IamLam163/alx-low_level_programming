f49d43221dc548b4b0cc5403e33b1e1cc87738aa
IamLam163 <olamidealiyu@gmail.com>
2022-06-03 17:30:35 -0700

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints single digit numbers
+ *
+ *     Return: 0
+ */
+
+int main(void)
+
+	{
+	int n = 48;
+
+	while (n <= 57)
+
+	{
+		putchar(n);
+
+		n += 1;
+	}
+	printf("\n");
+
+	return (0);
+	}
