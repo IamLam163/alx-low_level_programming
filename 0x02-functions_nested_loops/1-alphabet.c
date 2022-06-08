b938e01c0d856ee14fd4b0f7883418237ef7900c
IamLam163 <olamidealiyu@gmail.com>
2022-06-08 13:49:07 -0700

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,16 @@
 /**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  *
- * return 0; if successful
- *
  */
 
 void print_alphabet(void)
 {
 	int b;
 
-	b = 'a';
-
-	while ('b' <= 'z')
+	for (b = 97; b <= 122; b++)
 	{
 		_putchar(b);
-
-		b++;
 	}
+
 	_putchar('\n');
 }
