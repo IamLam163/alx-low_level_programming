a213590bf507b886a5741792efebdb3fd8363ff6
IamLam163 <olamidealiyu@gmail.com>
2022-06-08 11:45:50 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - program that prints _putchar
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	_putchar('95');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
