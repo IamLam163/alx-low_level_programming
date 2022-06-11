51635d85520078b7431b96c9fd73042c24abb450
IamLam163 <olamidealiyu@gmail.com>
2022-06-11 08:39:20 -0700

    update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 int _putchar(char c);
 
 int _isupper(int c);
@@ -13,4 +13,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 
 void more_numbers(void);
+
+void print_line(int n);
 #endif
