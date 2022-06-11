6e638e603c365ac89ac7d156a5c548712361abea
IamLam163 <olamidealiyu@gmail.com>
2022-06-11 07:29:30 -0700

    more numbers task6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  more_numbers - program prints numbers from 0 to 14
+ *  
+ *  Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int b, u;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (u = 0; u < 15; u++)
+		
+			if (u >= 10)
+				_putchar((u / 10) + 48);
+			
+			_putchar((u % 10) + 48);
+		_putchar('\n');
+	
+	}
+}
