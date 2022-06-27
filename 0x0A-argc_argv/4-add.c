bc69f68e4739ddc7ac2d92dcc3eba8a24d086a72
IamLam163 <olamidealiyu@gmail.com>
2022-06-26 19:27:01 -0700

    betty is annoying

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that adds two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int n = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+		if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		n += atoi(argv[a]);
+	}
+	printf("%d\n", n);
+	return (0);
+}
+
+
