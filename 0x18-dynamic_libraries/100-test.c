67c14f8686e20a2a882c03b3b4f09fd41e818e79
IamLam163 <olamidealiyu@gmail.com>
2022-09-05 15:16:53 -0700

    task 3

---
+++ 0x18-dynamic_libraries/100-test.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - multiply two variables
+ * @a: variable 1
+ * @b: variable 2
+ * Return: addition of thwo variables
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two variables
+ * @a: variable 1
+ * @b: variable 2
+ * Return: subtraction of two variables
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *  mul - multiply two variables
+ *  @a: var 1
+ *  @b: var 2
+ *  Return: mul of two var
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: var 1
+ * @b: var 2
+ * Return: division of two var
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - find the modulus
+ * @a: var 1
+ * @b: var 2
+ * Return: modulusof tow numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
